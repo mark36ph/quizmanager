de838e38bcb04302f9ed8e71fb7b5a85f9dc12e3
Mark <mark36ph@gmail.com>
2026-09-04 12:53:17 +0100

    Open functional question library from shell

--- src/QuizManager.Desktop/MainWindow.xaml.cs
+++ src/QuizManager.Desktop/MainWindow.xaml.cs
@@ -1,9 +1,23 @@
+using QuizManager.Infrastructure.Data;
+
 namespace QuizManager.Desktop;
 
 public partial class MainWindow : System.Windows.Window
 {
-    public MainWindow()
+    private readonly QuestionLibraryService _questionLibrary;
+
+    public MainWindow(QuestionLibraryService questionLibrary)
     {
+        _questionLibrary = questionLibrary;
         InitializeComponent();
+    }
+
+    private void QuestionLibrary_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var window = new QuestionLibraryWindow(_questionLibrary)
+        {
+            Owner = this
+        };
+        window.ShowDialog();
     }
 }
